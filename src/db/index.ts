480e3a75c1c95acaff6e4c7a330235e890490f80
dengnh <dengninghai@shinemo.com>
2022-08-10 10:01:43 +0800

    feat：增加环境变量设置

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
+const mode = process.env.NODE_ENV;
+
+const dbpath =
+  mode === "dev"
+    ? "mongodb://localhost:27017/blog_web"
+    : "mongodb://47.97.107.28:27017/blog_web";
+
 const connectMongodb = () => {
   mongoose
-    .connect("mongodb://localhost:27017/blog_web")
+    .connect(dbpath)
     .then(() => {
       console.log("数据库连接成功！");
     })
