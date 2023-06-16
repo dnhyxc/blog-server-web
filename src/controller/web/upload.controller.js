e675d850a7665a2f2eeab01bdb47b9cda515d219
dengnh <dengninghai@shinemo.com>
2023-06-16 11:36:07 +0800

    feat：支持批量删除文件

--- src/controller/web/upload.controller.js
+++ src/controller/web/upload.controller.js
@@ -28,37 +28,42 @@ class UploadController {
    */
   async removeFileCtr(ctx, next) {
     const { url } = ctx.request.body;
-    const index = url.lastIndexOf("/");
-    const sliceUrl = url.substring(index + 1, url.length);
-    const filePath = path.normalize(`${publicPath}/${sliceUrl}`);
-    // 判断文件是否存在
-    if (fs.existsSync(filePath)) {
+
+    const urls = url && Array.isArray(url) ? url : [url];
+
+    urls.forEach((url) => {
+      const index = url.lastIndexOf("/");
+      const sliceUrl = url.substring(index + 1, url.length);
+      const filePath = path.normalize(`${publicPath}/${sliceUrl}`);
       try {
-        const stats = fs.statSync(filePath);
-        // 判断是否是文件
-        if (stats?.isFile()) {
-          // 删除文件
-          fs.unlinkSync(filePath);
+        // 判断文件是否存在
+        if (fs.existsSync(filePath)) {
+          const stats = fs.statSync(filePath);
+          // 判断是否是文件
+          if (stats?.isFile()) {
+            // 删除文件
+            fs.unlinkSync(filePath);
+          }
+          // 判断是否是文件夹
+          // if (stats.isDirectory()) {
+          //   let filesArr = fs.readdirSync(filePath);
+          //   filesArr.forEach((file) => {
+          //     removeFiles(path.resolve(filePath, file));
+          //   });
+          //   fs.rmdirSync(filePath);
+          // }
           ctx.body = {
             code: 200,
             message: "文件删除成功",
             success: true,
           };
+        } else {
+          ctx.app.emit("error", fileNotFound, ctx);
         }
       } catch (error) {
         ctx.app.emit("error", fileNotFound, ctx);
       }
-      // 判断是否是文件夹
-      // if (stats.isDirectory()) {
-      //   let filesArr = fs.readdirSync(filePath);
-      //   filesArr.forEach((file) => {
-      //     removeFiles(path.resolve(filePath, file));
-      //   });
-      //   fs.rmdirSync(filePath);
-      // }
-    } else {
-      ctx.app.emit("error", fileNotFound, ctx);
-    }
+    });
   }
 
   // 大文件上传
